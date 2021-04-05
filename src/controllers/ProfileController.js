1b4ef6308dcfb47dd17e33ae4662cc56e32d660b
Athos Chaves <a.thos_chaves@hotmail.com>
2021-04-06 00:55:49 +0200

    lektion 3

---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,36 @@
+
+const Profile = require('../model/Profile')
+
+module.exports = {
+    index(req, res) {
+        return res.render("profile", { profile: Profile.get() })
+    },
+
+    update(req, res) {
+       //req.body para pegar os dados
+       const data = req.body
+
+       //definir quantas semanas tem no ano: 52
+       const weeksPerYear = 52
+
+       //remover as semnas de ferias do ano, para pegar quantas semanas tem em 1 mes
+       const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+
+       // total de horas trabalahdas por semana 
+       const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+       //horas trabalhadas no mes
+       const monthlyTotalHours = weekTotalHours * weeksPerMonth
+       
+       //qual será o valor da minha hora ?
+       const valueHour = data["value-hour"] = data["monthly-budget"] / monthlyTotalHours
+
+       Profile.update({
+        ...Profile.get(),
+        ...req.body,
+        "value-hour": valueHour
+       })
+
+       return res.redirect('/profile')
+    }
+};
--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const server = express()
 const routes = require("./routes.js")
+const path = require("path")
 
 //Embedded JavaScript templating (EJS) = template engine.
 server.set('view engine', 'ejs')
+
+//Mudar a localizacao da pasta views
+server.set('views', path.join(__dirname, 'views'))
 
 //habilitar arquivos estaticos
 server.use(express.static("public"))
